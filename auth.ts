7e218d1fcc532c39c8f75c3469a09f5a45093fb7
Riukim <m.cominelli29@gmail.com>
2024-06-25 15:22:51 +0200

    Add user role to prisma schema, add callbacks

--- auth.ts
+++ auth.ts
@@ -1,13 +1,50 @@
-import NextAuth from "next-auth"
+import NextAuth, { type DefaultSession } from "next-auth"
+
+import { JWT } from "next-auth/jwt"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import authConfig from "./auth.config"
+import { getUserById } from "./data/user"
 import { db } from "./lib/db"
 
-export const {
-  auth,
-  handlers,
-  signIn,
-  signOut } = NextAuth({
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: "ADMIN" | "USER"
+    } & DefaultSession
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: "ADMIN" | "USER"
+  }
+}
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role
+      }
+
+      return session
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) return token
+
+      token.role = existingUser.role
+
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
