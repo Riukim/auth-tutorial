1304435d799a67c740f39fb727b08cec168948a4
Riukim <m.cominelli29@gmail.com>
2024-06-25 09:24:47 +0200

    Implement login and mock logout

--- auth.ts
+++ auth.ts
@@ -3,7 +3,11 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 import authConfig from "./auth.config"
 import { db } from "./lib/db"
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
+export const {
+  auth,
+  handlers: {GET, POST},
+  signIn,
+  signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
