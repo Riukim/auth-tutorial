34891f27c1a1c5d4ee5f92639f940a274d753e59
Riukim <m.cominelli29@gmail.com>
2024-06-27 10:26:35 +0200

    Implement reset password actions

--- components/auth/ResetForm.tsx
+++ components/auth/ResetForm.tsx
@@ -6,7 +6,7 @@ import { useState, useTransition } from "react"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 
-import { LoginSchema } from "@/schemas"
+import { ResetSchema } from "@/schemas"
 
 import {
   Form,
@@ -22,34 +22,26 @@ import { Input } from "../ui/input"
 import { Button } from "../ui/button"
 import FormError from "../FormError"
 import FormSuccess from "../FormSuccess"
-import { login } from "@/actions/login"
-import { useSearchParams } from "next/navigation"
-import Link from "next/link"
+import { reset } from "@/actions/reset"
 
-const LoginForm = () => {
-  const searchParams = useSearchParams()
-  const urlError = searchParams.get("error") === "OAuthAccountNotLinked"
-    ? "Email already in use with a different provider"
-    : ""
-
+const ResetForm = () => {
   const [error, setError] = useState<string | undefined>("")
   const [success, setSuccess] = useState<string | undefined>("")
   const [isPending, startTransition] = useTransition()
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof ResetSchema>>({
+    resolver: zodResolver(ResetSchema),
     defaultValues: {
       email: "",
-      password: "",
     },
   })
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof ResetSchema>) => {
     setError("")
     setSuccess("")
 
     startTransition(() => {
-      login(values)
+      reset(values)
         .then((data) => {
           setError(data?.error)
           setSuccess(data?.success)
@@ -59,10 +51,9 @@ const LoginForm = () => {
 
   return (
     <CardWrapper
-      headerLabel="Welcome back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
-      showSocial
+      headerLabel="Forgot your password?"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
     >
       <Form {...form}>
         <form
@@ -88,43 +79,16 @@ const LoginForm = () => {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      type="password"
-                      placeholder="******"
-                    />
-                  </FormControl>
-                  <Button
-                    size="sm"
-                    variant="link"
-                    asChild
-                    className="px-0 font-normal"
-                  >
-                    <Link href="/auth/reset">
-                      Forgot Password?
-                    </Link>
-                  </Button>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            
           </div>
-          <FormError message={error || urlError} />
+          <FormError message={error} />
           <FormSuccess message={success} />
           <Button
             type="submit"
             className="w-full"
             disabled={isPending}
           >
-            Login
+            Send reset email
           </Button>
         </form>
       </Form>
@@ -132,4 +96,4 @@ const LoginForm = () => {
   )
 }
 
-export default LoginForm
+export default ResetForm
--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,4 +1,5 @@
 import { getVerficationTokenByEmail } from "@/data/verificationToken"
+import { getPasswordResetTokenByEmail } from "@/data/passwordResetToken"
 import { v4 as uuidv4 } from "uuid"
 import { db } from "./db"
 
@@ -25,4 +26,27 @@ export const generateVerifcationToken = async (email: string) => {
   })
 
   return verifcationToken
+}
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4()
+  const expires = new Date(new Date().getTime() + 3600 * 1000)
+
+  const existingToken = await getPasswordResetTokenByEmail(email)
+
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: { id: existingToken.id },
+    })
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires
+    }
+  })
+
+  return passwordResetToken
 }
