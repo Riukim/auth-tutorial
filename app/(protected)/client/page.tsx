5f76ecd6be5c358233b4e41e632faa488e13eb51
Riukim <m.cominelli29@gmail.com>
2024-06-28 11:39:43 +0200

    Implemnt client page

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { UserInfo } from "@/components/UserInfo";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+
+const ClientPage = () => {
+  const user = useCurrentUser()
+
+  return ( 
+    <div>
+      <UserInfo
+        user={user}
+        label="📱 Client Component"
+      />
+    </div>
+   );
+}
+ 
+export default ClientPage;
